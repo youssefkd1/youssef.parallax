f2465f7c3842603e21c0553172cf16f7e40e6b3c
youssefkd1 <yousseffiras555@gmail.com>
2025-04-18 19:19:01 +0200

    1

--- action.js
+++ action.js
@@ -62,20 +62,13 @@ var links = document.getElementsByClassName("social-media");
 
 links[0].addEventListener("click", () => { openlink(1) });
 links[1].addEventListener("click", () => { openlink(2) });
-links[2].addEventListener("click", () => { openlink(3) });
-links[3].addEventListener("click", () => { openlink(4) });
+
 
 function openlink(x) {
     if (x == 1) {
-        window.open("https://www.instagram.com/_.vini._02_/", "_blank");
+        window.open("https://github.com/youssefkd1", "_blank");
     }
     if (x == 2) {
-        window.open("https://www.linkedin.com/in/vineet-kumar-gupta-2833ab196/", "_blank");
-    }
-    if (x == 3) {
-        window.open("https://github.com/VineetKumar02", "_blank");
-    }
-    if (x == 4) {
-        window.open("https://vineet-portfolio-site.netlify.app/", "_blank");
+        window.open("https://soon/", "_blank");
     }
 }
